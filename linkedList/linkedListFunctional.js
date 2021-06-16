c7777cb6cd3e62e518a39559012bef6a2638f291
Alexander Zinn <alexzinnde@gmail.com>
2021-06-16 09:49:59 -0400

    LinkedList functional pattern

---
+++ linkedList/linkedListFunctional.js
@@ -0,0 +1,51 @@
+const Node = (value) => {
+  const node = {};
+
+  node.value = value;
+  node.next = null;
+
+  return node;
+};
+
+const LinkedList = function LinkedList() {
+  const list = {};
+  list.head = null;
+  list.tail = null;
+
+  // Time Complexity: O(1)
+  list.addToTail = (value) => {
+    const node = Node(value);
+
+    if (list.tail === null) {
+      list.head = node;
+      list.tail = node;
+      return;
+    }
+
+    list.tail.next = node;
+    list.tail = node;
+  };
+
+
+  // Time Complexity: O(1)
+  list.removeHead = () => {
+    const oldHead = list.head;
+    list.head = list.head.next;
+    oldHead.next = null;
+    return oldHead.value;
+  };
+
+  // Time Complexity O(n)
+  list.contains = (target) => {
+    let node = list.head;
+    while (node) {
+      if (node.value === target) {
+        return true;
+      }
+      node = node.next;
+    }
+    return false;
+  };
+
+  return list;
+};
