6e6f8b688d272ad74d8a4eab8d3c26c381b3e241
Alexander Zinn <alexzinnde@gmail.com>
2021-07-07 15:30:23 -0400

    quick sort

--- Sorting/QuickSort/quickSort.js
+++ Sorting/QuickSort/quickSort.js
@@ -1,19 +1,28 @@
+/* eslint-disable no-console */
+/* eslint-disable no-plusplus */
 /**
  * pick a random element
  *  place all the elements in the input array into two lists
  *  nums greater and less than pivot
  *  call quick sort on the two lists
+ *
+ * Time complexity:
+ * - Worst Case:O(N logN)
+ * - Avg Case:  O(N logN)
+ * - Best Case: O(N logN)
+ *
+ * Space Complexity:
+ * - O(logN)
  */
 
 const quickSort = (arr) => {
   if (arr.length < 2) {
     return arr;
   }
-  const pivot = arr.splice(0, 1);
-  const left = arr.slice().filter((x) => x < pivot);
-  const right = arr.slice().filter((x) => x >= pivot);
-
-  return [].concat(quickSort(left), pivot, quickSort(right));
+  const pivot = arr[0];
+  const left = quickSort(arr.slice(1).filter((x) => x < pivot));
+  const right = quickSort(arr.slice(1).filter((x) => x >= pivot));
+  return [].concat(left, pivot, right);
 };
 
 function test() {
